aed2e16171babd0c4a74ab607186b71ef608ad08
Rahul Jha <rahuljha@umich.edu>
2014-05-06 14:28:15 -0400

    missed

--- SurveyorData.py
+++ SurveyorData.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from nltk.tokenize import wordpunct_tokenize
 import itertools
 from string import punctuation
+from collections import defaultdict
 
 import numpy as np
 
@@ -18,6 +19,9 @@ class SurveyorData(Data):
         """
 
         self.topic_doc_sents = defaultdict(dict)
+        self.topic_doc_origs = defaultdict(dict)
+        self.topic_doc_vecs = defaultdict(dict)
+
         self.vocab = []
         # read data from files and update topic_doc_sents and vocab, topic_doc_sentences now contains a hash from topics to docs to words
         self.read_data(topics, data_root)
@@ -29,6 +33,8 @@ class SurveyorData(Data):
         self.sent_vecs = np.zeros((numsents, len(self.vocab)), dtype=np.int)
         self.sent2docsets = np.zeros(numsents, dtype=np.int)
         self.sent2docs = np.zeros(numsents, dtype=np.int)
+
+        self.docsets2sents = defaultdict(list)
         
         # updates self.doctopic2sent and self.numsents
         self.build_vectors()
@@ -41,6 +47,7 @@ class SurveyorData(Data):
                 topic_idx = self.docsets.index(topic)
                 doc_idx = self.docs.index(doc)
                 
+                self.topic_doc_vecs[topic][doc] = []
                 for sent in self.topic_doc_sents[topic][doc]:
                     for word in sent:
                         word_idx = self.vocab.index(word)
@@ -48,8 +55,9 @@ class SurveyorData(Data):
 
                     self.sent2docsets[sent_idx] = topic_idx
                     self.sent2docs[sent_idx] = doc_idx
-                    sent_idx += 1
+                    self.topic_doc_vecs[topic][doc].append(self.sent_vecs[sent_idx])
 
+                    sent_idx += 1
 
         assert len(self.sent2docs) == sent_idx
 
@@ -63,10 +71,12 @@ class SurveyorData(Data):
                 pid = os.path.basename(infile).replace(".txt", "")
                 pid = topic+"_"+pid # this is done so that the same document appearing in two different topics will be treated as duplicates
                 self.topic_doc_sents[topic][pid] = []
+                self.topic_doc_origs[topic][pid] = []
                 infh = open(infile, "r")
                 for line in infh:
                     words = [w.strip(punctuation).lower() for w in wordpunct_tokenize(line.strip()) if w.strip(punctuation) != ""]
                     self.topic_doc_sents[topic][pid].append(words)
+                    self.topic_doc_origs[topic][pid].append(line.strip())
                     for w in words:
                         vocab_hash[w] = 1
 
